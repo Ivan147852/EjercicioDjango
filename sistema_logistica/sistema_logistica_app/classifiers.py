8514160add270a50a11f9361146184c84fd44839
Ivan147852 <ivan1alberghini@gmail.com>
2023-08-14 18:12:46 -0700

    Cuarto commit

--- sistema_logistica/sistema_logistica_app/classifiers.py
+++ sistema_logistica/sistema_logistica_app/classifiers.py
@@ -21,57 +21,52 @@ class SimpleClassifier(PackageClassifier):
         self.classification = classification
         self.classificationAttribute = classificationAttribute
 
+    def classify(self, package, *args):
+        for attribute in args:
+            if attribute == self.classificationAttribute.value:
+                if self.compare(getattr(package, self.classificationAttribute.value)):
+                    return self.classification
+                elif self.nextClassifier:
+                    return self.nextClassifier.classify(package, *args)
+        return None
+    
+    @abstractmethod
+    def compare(self, attribute_value):
+        pass
+
+class LowerClassifier(SimpleClassifier):
+    def compare(self, attribute_value):
+        return attribute_value < self.attributeValue
+
+class HigherClassifier(SimpleClassifier):
+    def compare(self, attribute_value):
+        return attribute_value > self.attributeValue
+
+class EqualClassifier(SimpleClassifier):
+    def compare(self, attribute_value):
+        return attribute_value == self.attributeValue
+    
+
 class CompositeClassifier(PackageClassifier):
     def __init__(self, classifier1, classifier2, classification, nextClassifier=None):
         super().__init__(nextClassifier)
         self.classifier1 = classifier1
         self.classifier2 = classifier2
         self.classification = classification
-
-
-class LowerClassifier(SimpleClassifier):
-    def classify(self, package, *args):
-        for attribute in args:
-            if attribute == self.classificationAttribute.value:
-                if getattr(package, self.classificationAttribute.value) < self.attributeValue:
-                    return self.classification
-                elif self.nextClassifier:
-                    return self.nextClassifier.classify(package,*args)
-        return None
-
-class HigherClassifier(SimpleClassifier):
-    def classify(self, package, *args):
-        for attribute in args:
-            if attribute == self.classificationAttribute.value:
-                if getattr(package, self.classificationAttribute.value) > self.attributeValue:
-                    return self.classification
-                elif self.nextClassifier:
-                    return self.nextClassifier.classify(package,*args)
-        return None
-
-class EqualClassifier(SimpleClassifier):
-    def classify(self, package, *args):
-        for attribute in args:
-            if attribute == self.classificationAttribute.value:
-                if getattr(package, self.classificationAttribute.value) > self.attributeValue:
-                    return self.classification
-                elif self.nextClassifier:
-                    return self.nextClassifier.classify(package,*args)
+    
+    def _classify_logic(self, package, *args, operator):
+        result1 = self.classifier1.classify(package, *args)
+        result2 = self.classifier2.classify(package, *args)
+        if (result1 is not None) and operator(result2):
+            return self.classification
+        elif self.nextClassifier:
+            return self.nextClassifier.classify(package, *args)
         return None
 
 class AndClassifier(CompositeClassifier):
     def classify(self, package, *args):
-        if (self.classifier1.classify(package, *args) != None) and (self.classifier2.classify(package, *args)):
-            return self.classification
-        elif self.nextClassifier:
-            return self.nextClassifier.classify(package,*args)
-        return None
+        return self._classify_logic(package, *args, operator=lambda x: bool(x))
 
 class OrClassifier(CompositeClassifier):
     def classify(self, package, *args):
-        if (self.classifier1.classify(package, *args) != None) or (self.classifier2.classify(package, *args)):
-            return self.classification
-        elif self.nextClassifier:
-            return self.nextClassifier.classify(package,*args)
-        return None
-
+        return self._classify_logic(package, *args, operator=lambda x: not bool(x))
